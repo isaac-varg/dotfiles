8bbd9e9b1c44cb0ee372a1eb7c3d7e1df1c0a01b
isaac-varg <hello@isaacvargas.dev>
2025-04-08 22:17:55 -0700

    add lazy aliases, fastfetch logos and config, change screenshot keybind

--- dot_config/zsh/alias.zsh
+++ dot_config/zsh/alias.zsh
@@ -1,8 +1,15 @@
 alias c="clear"
 
+# i swear i am not lazy
+alias ff="fastfetch"
+alias n="nvim"
+alias lg="lazygit"
+alias y="yazi"
+
 # chezmoi aliases
 alias cza="chezmoi apply"
 alias czd="chezmoi cd"
+
 # useful while configuring waybar
 alias ww="chezmoi apply && waybar"
 
