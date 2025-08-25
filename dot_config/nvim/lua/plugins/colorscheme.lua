4532735607ca4b41041a00a3c9c7882860cdc5d3
Isaac Vargas <hello@isaacvargas.dev>
2025-08-25 16:39:27 -0700

    fix bufferline again?

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,8 +3,8 @@ return {
     "catppuccin/nvim",
     lazy = true,
     name = "catppuccin",
+    flavor = "mocha",
     opts = {
-      flavor = "mocha",
       integrations = {
         aerial = true,
         alpha = true,
@@ -44,25 +44,14 @@ return {
         which_key = true,
       },
     },
-    specs = {
-      {
-        "akinsho/bufferline.nvim",
-        optional = true,
-        opts = function(_, opts)
-          if (vim.g.colors_name or ""):find("catppuccin") then
-            opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-          end
-        end,
-      },
+    {
+      "akinsho/bufferline.nvim",
+      optional = true,
+      opts = function(_, opts)
+        if (vim.g.colors_name or ""):find("catppuccin") then
+          opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
+        end
+      end,
     },
-  },
-  {
-    "akinsho/bufferline.nvim",
-    optional = true,
-    opts = function(_, opts)
-      if (vim.g.colors_name or ""):find("catppuccin") then
-        opts.highlights = require("catppuccin.groups.integrations.bufferline").get()
-      end
-    end,
   },
 }
